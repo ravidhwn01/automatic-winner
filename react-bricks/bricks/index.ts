90479c37ae8082fd286775f20f514c1d616e8d5d
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-12-05 16:31:53 +0530

    added dark mode and some feature

--- react-bricks/bricks/index.ts
+++ react-bricks/bricks/index.ts
@@ -2,8 +2,6 @@ import { types } from 'react-bricks/frontend'
 import website from 'react-bricks-ui/website'
 import blog from 'react-bricks-ui/blog'
 import HeroUnit from './MyHeroUnit'
-
-
 const bricks: types.Brick<any>[] = [
   ...website, // React Bricks UI
   ...blog,
